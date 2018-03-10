f7b19a6a6e6df1a06a6a465a8af16d9646921a1b
tracer <tracer@tracer.org>
2018-03-10 13:46:50 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
+#include<dirent.h>
 
-int main(int argc, char *argv[]) {
-  int i;
-  for (i = 0; i < argc; i++) {
-    assert(argv[i]); // specification
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]); // specification
+int main(int argc, char *argv[]) 
+{
+	int i;
+	for (i = 0; i < argc; i++) 
+	{
+		assert(argv[i]); // specification
+	    printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	assert(!argv[argc]); // specification
+	DIR *dirptr=NULL;
+    int i=1;
+    struct dirent *entry;
+    if((dirptr = opendir("/proc"))==NULL)
+    {
+        printf("opendir failed!");
+        return 1;
+    }
+    else
+    {
+        while(entry=readdir(dirptr))
+        {
+    	    printf("filename%d=%s\n",i,entry->d_name);
+    	    i++;
+        }
+    	closedir(dirptr);
+    }
   return 0;
 }
