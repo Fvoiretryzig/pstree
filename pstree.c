98b2bf1c9c63ecd234c4a959b9a8557f6bf2bd83
tracer <tracer@tracer.org>
2018-03-11 21:00:40 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -31,6 +31,9 @@ void remove_space(char *s)
 			*s ++= *temp;
 		temp++;
 	}
+	char print_temp[256];
+	strcpy(print_temp, temp);
+	printf("print_temp:%s\n", print_temp);
 	*s = '\0';
 	return;
 }
@@ -68,7 +71,7 @@ void save_info(char* dirname)
 	FILE* pstree_file;
 	/*--------打开每个进程里面的status--------*/
 	strcpy(filename, dirname);
-	printf("filename:%s\n", filename);
+	//printf("filename:%s\n", filename);
 	strcat(filename, "/status");
 	pstree_file = fopen(filename, "r");
 	if(pstree_file == NULL)
