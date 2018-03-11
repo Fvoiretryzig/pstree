d0a1d97bb9f57b8f312c096eb4dffc58731c61fa
tracer <tracer@tracer.org>
2018-03-11 21:02:38 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -34,7 +34,7 @@ void remove_space(char *s)
 	char print_s[256];
 	strcpy(print_s, s);
 	*s += '\0';
-	printf("print_s:%s\n", print_s);
+	printf("print_s:%s %s\n", print_s, s);
 	return;
 }
 /*----------建链表为了以后建树----------*/
