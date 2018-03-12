1ad5661652dae06618cff7c29cd1aaf332170c3e
tracer <tracer@tracer.org>
2018-03-12 20:31:30 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -145,16 +145,14 @@ void print_tree(int option, struct pstree_node *root, int layer)
 	struct pstree_node *temp;
 	for(int i = 1; i<=layer; i++)
 		printf("	");
-	//struct pstree_node *print_temp;
-	if(root->parent!=NULL)
-	printf("len:%d\n", strlen(root->parent->name));
-	/*while(root->parent!=NULL)
+	struct pstree_node *print_temp;
+	while(root->parent!=NULL)
 	{
 		print_temp = root->parent;
 		int len = strlen(print_temp->name);
-		for(int i = 0; i<len; i++)
+		for(int i = 0; i<len-1; i++)
 			printf(" ");
-	}*/
+	}
 	if(layer)
 		printf("|--");
 	switch(option)
