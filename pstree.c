f445af9b4e1320e377042f3901e812eed691345b
tracer <tracer@tracer.org>
2018-03-14 21:43:43 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -96,7 +96,7 @@ void save_info(char* dirname)
 		//content = strtok(NULL, ":");
 		//strcpy(content, buffer+strlen(header));
 		printf("this is before cpy\n");
-		strncpy(content, &buffer[0]+strlen(header)-1, strlen(buffer) - strlen(header));
+		strncpy(content, buffer+strlen(header)-1, 3);
 		printf("content:%s\n", content);
 		if(header != NULL && content != NULL)
 		{
